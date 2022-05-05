7c6abc4aa90ee85fd7a4f88dd73d9fd8bd8a997c
Chinwendu <chinwenduukoha@gmail.com>
2022-05-05 15:44:25 -0400

    admin created

--- unicorn_project/unicorn/admin.py
+++ unicorn_project/unicorn/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import User, GriefImage, GriefStage, Comments, Community, Discussion
+
+admin.site.register(User)
+admin.site.register(GriefStage)
+admin.site.register(GriefImage)
+admin.site.register(Comments)
+admin.site.register(Community)
+admin.site.register(Discussion)
